7041fb5d2804a90ae0c502193775cfce0f26a8a3
TenpreA <1953315139@qq.com>
2026-07-22 22:23:46 +0800

    Add files via upload

---
+++ _next/static/y6j9OFgKQux5UFN7nwnLi/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/wuxu//_next/:path+",
+        "destination": "/wuxu/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
